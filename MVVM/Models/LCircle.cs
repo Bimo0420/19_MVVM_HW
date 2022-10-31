01930e6b40921c3ed3abe7e0560e2d389010cd53
Алексей Викулин <vaa@megamade.ru>
2022-10-31 19:32:37 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Models/LCircle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Models
+{
+    static class LCircle
+    {
+       public static double Add(double a) => a * 2 * 3.14;
+    }
+}
